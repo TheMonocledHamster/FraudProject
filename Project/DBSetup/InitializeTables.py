c9e989b2467e967c2065ce31caea6aa8198feb15
TheMonocledHamsterOnAdiPC <adithyah07@gmail.com>
2022-01-09 00:46:07 +0530

    Add safe psycopg2-conection

--- Project/DBSetup/InitializeTables.py
+++ Project/DBSetup/InitializeTables.py
@@ -3,6 +3,8 @@ import Const
 
 params = Const.PARAMS
 pg_connection = psycopg2.connect(dbname=params["NAME"], user=params["USER"], password=params["PASSWORD"], host=params["HOST"], port=params["PORT"])
-pg_connection.set_session(autocommit=True)
 db_cursor = pg_connection.cursor()
 db_cursor.execute(Const.SCHEMA)
+pg_connection.commit()
+db_cursor.close()
+pg_connection.close()
