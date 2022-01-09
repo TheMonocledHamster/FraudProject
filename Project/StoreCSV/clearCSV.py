8ee63306c7e3b80b51cde1369ea20a0258cfd969
TheMonocledHamsterOnAdiPC <adithyah07@gmail.com>
2022-01-09 13:45:19 +0530

    Cleanup

--- Project/StoreCSV/clearCSV.py
+++
@@ -1,8 +0,0 @@
-import os
-
-FILE_LIST = ["plans.csv", "features.csv", "subscribers.csv", "tracking.csv", "transactions.csv", "usage_data.csv"]
-relpath = lambda p: os.path.normpath(os.path.join(os.path.dirname(__file__), p))
-
-for file in FILE_LIST:
-    with open( relpath(file),'r+') as csvfile:
-        csvfile.truncate(0)
