9ae5282c45d161055c1a3a1d0e14bbe014207c3f
TheMonocledHamsterOnAdiPC <adithyah07@gmail.com>
2022-01-10 01:21:56 +0530

    DBSCAN

--- Project/DBSCAN/Scan.py
+++ Project/DBSCAN/Scan.py
@@ -15,7 +15,9 @@ with open(relpath("..\..\Resources\DBdetails.json"),"r") as deets:
 pg_conn=psycopg2.connect(dbname=params["NAME"], user=params["USER"], password=params["PASSWORD"], host=params["HOST"], port=params["PORT"])
 
 """ Load Data """
-X = pd.read_sql("SELECT amount FROM usage_data WHERE use_type = ",pg_conn)
+X = pd.read_sql("SELECT sub_id, amount, COUNT(sub_id) FROM usage_data WHERE use_type='SMS' GROUP BY sub_id,amount",pg_conn)
+
+X = X.drop('sub_id',axis=1)
 X.fillna(method='ffill', inplace=True)
 
 
@@ -27,7 +29,31 @@ X_scaled = scaler.fit_transform(X)
 X_normalized = normalize(X_scaled)
 X_normalized = pd.DataFrame(X_normalized)
 
-db_def = DBSCAN(eps=0.5, min_samples=20000)
+""" Dimensional Fitting """
+pca = PCA(n_components = 2)
+X_principal = pca.fit_transform(X_normalized)
+X_principal = pd.DataFrame(X_principal)
+X_principal.columns = ['P1', 'P2']
+
+db_def = DBSCAN(eps=0.3, min_samples=300).fit(X_principal)
 labels = db_def.labels_
 
+colors = {}
+colors[0] = 'r'
+colors[1] = 'g'
+colors[2] = 'b'
+colors[-1] = 'k'
 
+color_vector = [colors[label] for label in labels]
+
+r = plt.scatter(X_principal['P1'], X_principal['P2'], color ='r')
+g = plt.scatter(X_principal['P1'], X_principal['P2'], color ='g')
+b = plt.scatter(X_principal['P1'], X_principal['P2'], color ='b')
+k = plt.scatter(X_principal['P1'], X_principal['P2'], color ='k')
+
+plt.figure(figsize=(9,9))
+plt.scatter(X_principal['P1'], X_principal['P2'], c=color_vector)
+
+plt.legend((r,g,b,k),('UG0','UG1','UG2','Frauds'))
+
+plt.show()
