a0a29a88e2dbb7712b6c388f08583ed85b1881e1
TheMonocledHamsterOnAdiPC <adithyah07@gmail.com>
2022-01-09 13:19:43 +0530

    Added DropAllTables Functionality

---
+++ Project/DBSetup/DropTables.py
@@ -0,0 +1,14 @@
+import psycopg2
+import Const
+
+query = ""
+for table in Const.TABLES:
+    query += "DROP TABLE IF EXISTS {};\n".format(table)
+
+params = Const.PARAMS
+pg_connection = psycopg2.connect(dbname=params["NAME"], user=params["USER"], password=params["PASSWORD"], host=params["HOST"], port=params["PORT"])
+db_cursor = pg_connection.cursor()
+db_cursor.execute(query=query)
+pg_connection.commit()
+db_cursor.close()
+pg_connection.close()
