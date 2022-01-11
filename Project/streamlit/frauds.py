54c6503dd0faaab81b2044033f3c042d88a26726
TheMonocledHamsterOnAdiPC <adithyah07@gmail.com>
2022-01-11 11:36:46 +0530

    Add DBSCAN result to frontend

--- Project/streamlit/frauds.py
+++ Project/streamlit/frauds.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from PIL import Image
 from streamlit.elements.number_input import Number
 import connect
 
@@ -14,7 +15,10 @@ def Frauds():
     fraudsDetails = pd.read_sql("SELECT * FROM frauds",conn)
     st.dataframe(fraudsDetails)
 
-    st.write('Frauds by Country')
     countryWise = pd.read_sql('SELECT fraud_locale AS Location,COUNT(*) AS Count FROM frauds GROUP BY fraud_locale',conn)
     countryFraudPie = px.pie(countryWise, values=countryWise['count'], names=countryWise['location'],title='Country-wise Frauds')
     st.plotly_chart(countryFraudPie)
+
+    st.write('DBSCAN Results')
+    DBSCAN = Image.open('../../Resources/DBSCAN.png')
+    st.image(DBSCAN,caption='Analysis')
